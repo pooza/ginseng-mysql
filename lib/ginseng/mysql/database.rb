4492406e72ff5e140952d64a48057ad621134cff
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-06-11 14:39:29 +0900

    #3

---
+++ lib/ginseng/mysql/database.rb
@@ -0,0 +1,76 @@
+require 'sequel'
+require 'mysql2'
+require 'facets/time'
+require 'singleton'
+
+module Ginseng
+  module MySQL
+    class Database
+      include Singleton
+      include Package
+      attr_reader :connection
+
+      def initialize
+        @config = config_class.instance
+        @logger = logger_class.new
+        dsn = database_class.dsn
+        raise Ginseng::DatabaseError, 'Invalid DSN' unless dsn.valid?
+        @connection = Sequel.connect(dsn.to_s)
+      rescue => e
+        @logger.error(error: e)
+        raise Ginseng::DatabaseError, e.message, e.backtrace
+      end
+
+      def escape_string(value)
+        return connection.escape(value)
+      end
+
+      def create_sql(name, params = {})
+        template = query_template_class.new(name)
+        template.params = params
+        return template.to_s
+      rescue => e
+        @logger.error(error: e, name: name, params: params)
+        raise Ginseng::DatabaseError, e.message, e.backtrace
+      end
+
+      def execute(name, params = {})
+        rows = nil
+        sql = nil
+        secs = Time.elapse do
+          sql = create_sql(name, params)
+          rows = @connection.fetch(sql).all.map(&:with_indifferent_access)
+        end
+        @logger.info(sql: sql, rows: rows.count, seconds: secs.round(3)) if loggable? || slow?(secs)
+        return rows
+      rescue => e
+        @logger.error(error: e, sql: sql)
+        raise Ginseng::DatabaseError, e.message, e.backtrace
+      end
+
+      alias exec execute
+
+      def loggable?
+        return environment_class.test?
+      end
+
+      def slow?(seconds)
+        return @config['/mysql/slow_query/seconds'] < seconds
+      end
+
+      def self.dsn
+        return DSN.parse(Config.instance['/mysql/dsn'])
+      rescue Ginseng::ConfigError
+        return nil
+      end
+
+      def self.connect
+        return instance
+      end
+
+      def self.config?
+        return dsn.present?
+      end
+    end
+  end
+end
---
+++ test/database_test.rb
@@ -0,0 +1,26 @@
+module Ginseng
+  module MySQL
+    class DatabaseTest < Test::Unit::TestCase
+      def setup
+        @db = Database.instance
+      end
+
+      def test_connection
+        assert(@db.connection.is_a?(Sequel::Database))
+      end
+
+      def test_escape_string
+        assert_equal(@db.escape_string('あえ'), 'あえ')
+        assert_equal(@db.escape_string(%(あえ")), %(あえ\"))
+        assert_equal(@db.escape_string(%(あえ')), %(あえ''))
+      end
+
+      def test_execute
+        assert(@db.execute('tables', {schema: 'information_schema'}).present?)
+        @db.execute('tables', {schema: 'information_schema'}).each do |row|
+          assert(row.is_a?(Hash))
+        end
+      end
+    end
+  end
+end
